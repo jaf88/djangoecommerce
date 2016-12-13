731fac1bf3c530d538e09c730a7bda5a7dad83e8
jaf88 <jaf.alves8@gmail.com>
2016-12-13 16:57:43 -0300

    Login and Logout

--- djangoecommerce/urls.py
+++ djangoecommerce/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth.views import login,logout
 
 from core import views
 from catalog import views as views_catalog
@@ -23,6 +24,8 @@ from catalog import views as views_catalog
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
 	url(r'^contato/$', views.contact, name='contact'),
+    url(r'^entrar/$', login, {'template_name': 'login.html'}, name='login'),
+    url(r'^sair/$', logout, {'next_page': 'index'}, name='logout'),
 	url(r'^catalogo/', include('catalog.urls', namespace='catalog')),
-    url(r'^admin/', admin.site.urls),   
+    url(r'^admin/', admin.site.urls),
 ]
